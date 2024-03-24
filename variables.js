425f3328087b86ff51d440971bca8d038d9e6a65
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-03-24 14:00:38 -0400

    Finished Variables and Datatypes Basic

---
+++ variables.js
@@ -0,0 +1,11 @@
+let age = 25;
+
+const name = 'Gyancarlos';
+const isRaining = true;
+
+const numArr = [1, 8, 25, 100, 150];
+
+const person = {
+    firstName: 'Gyancarlos',
+    lastName: 'Pinto',
+}
